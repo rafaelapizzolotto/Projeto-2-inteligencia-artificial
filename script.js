ba94eaecfdedfea702d9fbce0e0349d22ced71cf
rafaelapizzolotto <164499957+rafaelapizzolotto@users.noreply.github.com>
2025-06-06 08:37:36 -0300

    .

--- script.js
+++ script.js
@@ -46,8 +46,15 @@ let atual = 0;
 let perguntaAtual;
 
 function mostraPergunta() {
-    perguntaAtual = perguntas[atual];
-    caixaPerguntas.textContent = perguntaAtual.enunciado;
+  perguntaAtual = perguntas[atual];
+  caixaPerguntas.textContent = perguntaAtual.enunciado;
+  mostraAlternativas();
 }
-
+function mostraAlternativas() {
+  for (const alternativa of perguntaAtual.alternativas) {
+    const botaoAlternativas = document.createElement("button");
+    botaoAlternativas.textContent = alternativa;
+    caixaAlternativas.appendChild(botaoAlternativas);
+  }
+}
 mostraPergunta();
